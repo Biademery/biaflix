15953d3c35d95736969d45a2b96b279f958ab4f1
Biademery <biademery@gmail.com>
2023-02-22 21:08:10 -0300

    add home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,95 +1,159 @@
 <template>
-  <div class="section">
-    <!-- <button class="myBtn" title="Go to top">Top</button> -->
-
-    <div id="action" class="cards">
-      <h2>
-        <router-link to="/action">
-          <a class="title">Ação</a>
-        </router-link>
-      </h2>
-      <div class="action genre"></div>
+  <div>
+    <h2>
+      <router-link to="/action">Ação</router-link>
+    </h2>
+    <div class="cards-home">
+      <li v-for="(movie, index) in movies" :key="index">
+        <div v-if="movie.genre == 'action'">
+          <a :href="movie.movieURL" target="_blank">
+            <img class="image" :src="movie.imageURL" alt="">
+          </a>
+        </div>
+      </li>
     </div>
-    <div id="adventure" class="cards">
-      <h2>
-        <router-link to="/adventure">
-          <a class="title">Aventura</a>
-        </router-link>
-      </h2>
-      <div class="adventure genre"></div>
+    <h2>
+      <router-link to="/adventure">Aventura</router-link>
+    </h2>
+    <div class="cards-home">
+      <li v-for="(movie, index) in movies" :key="index">
+        <div v-if="movie.genre == 'adventure'">
+          <a :href="movie.movieURL" target="_blank">
+            <img class="image" :src="movie.imageURL" alt="">
+          </a>
+        </div>
+      </li>
     </div>
-    <div id="comedy" class="cards">
-      <h2>
-        <router-link to="/comedy">
-          <a class="title">Comédia</a>
-        </router-link>
-      </h2>
-      <div class="comedy genre"></div>
+    <h2>
+      <router-link to="/comedy">Comédia</router-link>
+    </h2>
+    <div class="cards-home">
+      <li v-for="(movie, index) in movies" :key="index">
+        <div v-if="movie.genre == 'comedy'">
+          <a :href="movie.movieURL" target="_blank">
+            <img class="image" :src="movie.imageURL" alt="">
+          </a>
+        </div>
+      </li>
     </div>
-    <div id="romantic-comedy" class="cards">
-      <h2>
-        <router-link to="romantic-comedy">
-          <a class="title">Comédia Romântica</a>
-        </router-link>
-      </h2>
-      <div class="romantic-comedy genre"></div>
+    <h2>
+      <router-link to="romantic-comedy">Comédia Romântica</router-link>
+    </h2>
+    <div class="cards-home">
+      <li v-for="(movie, index) in movies" :key="index">
+        <div v-if="movie.genre == 'romantic-comedy'">
+          <a :href="movie.movieURL" target="_blank">
+            <img class="image" :src="movie.imageURL" alt="">
+          </a>
+        </div>
+      </li>
     </div>
-    <div id="drama" class="cards">
-      <h2>
-        <router-link to="/drama">
-          <a class="title">Drama</a>
-        </router-link>
-      </h2>
-      <div class="drama genre"></div>
+    <h2>
+      <router-link to="/drama">Drama</router-link>
+    </h2>
+    <div class="cards-home">
+      <li v-for="(movie, index) in movies" :key="index">
+        <div v-if="movie.genre == 'drama'">
+          <a :href="movie.movieURL" target="_blank">
+            <img class="image" :src="movie.imageURL" alt="">
+          </a>
+        </div>
+      </li>
     </div>
-    <div id="fantasy" class="cards">
-      <h2>
-        <router-link to="/fantasy">
-          <a class="title">Fantasia</a>
-        </router-link>
-      </h2>
-      <div class="fantasy genre"></div>
+    <h2>
+      <router-link to="/fantasy">Fantasia</router-link>
+    </h2>
+    <div class="cards-home">
+      <li v-for="(movie, index) in movies" :key="index">
+        <div v-if="movie.genre == 'fantasy'">
+          <a :href="movie.movieURL" target="_blank">
+            <img class="image" :src="movie.imageURL" alt="">
+          </a>
+        </div>
+      </li>
     </div>
-    <div id="sci-fi" class="cards">
-      <h2>
-        <router-link to="/sci-fi">
-          <a class="title">Ficção Científica</a>
-        </router-link>
-      </h2>
-      <div class="sci-fi genre"></div>
+    <h2>
+      <router-link to="/sci-fi">Ficção Científica</router-link>
+    </h2>
+    <div class="cards-home">
+      <li v-for="(movie, index) in movies" :key="index">
+        <div v-if="movie.genre == 'sci-fi'">
+          <a :href="movie.movieURL" target="_blank">
+            <img class="image" :src="movie.imageURL" alt="">
+          </a>
+        </div>
+      </li>
     </div>
-    <div id="musical" class="cards">
-      <h2>
-        <router-link to="/musical">
-          <a class="title">Musical</a>
-        </router-link>
-      </h2>
-      <div class="musical genre"></div>
+    <h2>
+      <router-link to="/musical">Musical</router-link>
+    </h2>
+    <div class="cards-home">
+      <li v-for="(movie, index) in movies" :key="index">
+        <div v-if="movie.genre == 'musical'">
+          <a :href="movie.movieURL" target="_blank">
+            <img class="image" :src="movie.imageURL" alt="">
+          </a>
+        </div>
+      </li>
     </div>
-    <div id="romance" class="cards">
-      <h2>
-        <router-link to="/romance">
-          <a class="title">Romance</a>
-        </router-link>
-      </h2>
-      <div class="romance genre"></div>
+    <h2>
+      <router-link to="/romance">Romance</router-link>
+    </h2>
+    <div class="cards-home">
+      <li v-for="(movie, index) in movies" :key="index">
+        <div v-if="movie.genre == 'romance'">
+          <a :href="movie.movieURL" target="_blank">
+            <img class="image" :src="movie.imageURL" :alt="movie.name">
+          </a>
+        </div>
+      </li>
     </div>
-    <div id="horror" class="cards">
-      <h2>
-        <router-link to="horror">
-          <a class="title">Suspense</a>
-        </router-link>
-      </h2>
-      <div class="horror genre"></div>
+    <h2>
+      <router-link to="horror">Terror</router-link>
+    </h2>
+    <div class="cards-home">
+      <li v-for="(movie, index) in movies" :key="index">
+        <div v-if="movie.genre == 'horror'">
+          <a :href="movie.movieURL" target="_blank">
+            <img class="image" :src="movie.imageURL" :alt="movie.name">
+          </a>
+        </div>
+      </li>
     </div>
   </div>
 </template>
 
 <script>
+import http from '../http-common.js';
+
+class MovieDataService {
+  getAll() {
+    return http.get('/movie/');
+  }
+}
 export default {
   name: "Home",
-};
+  data() {
+    return {
+      movies: [],
+    };
+  },
+  methods: {
+    addMovies() {
+      const movieDataService = new MovieDataService
+      movieDataService
+        .getAll()
+        .then((response) => {
+          this.movies = response.data;
+          console.log(response.data)
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+  },
+  mounted() {
+    this.addMovies();
+  },
+}
 </script>
-
-<style>
-</style>
