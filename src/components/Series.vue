648a96ffdb214448488f02435a94b5938cae1eda
Biademery <biademery@gmail.com>
2023-02-22 21:09:47 -0300

    add series

--- src/components/Series.vue
+++ src/components/Series.vue
@@ -1,8 +1,49 @@
-<template></template>
+<template>
+    <div class="body">
+        <h2>Séries</h2>
+        <div class="cards">
+            <li v-for="(movie, index) in movies" :key="index">
+                <div v-if="movie.type == 'serie'">
+                    <a :href="movie.movieURL" target="_blank">
+                        <img class="image" :src="movie.imageURL" :alt="movie.name">
+                    </a>
+                </div>
+            </li>
+        </div>
+    </div>
+</template>
 
 <script>
-export default {};
-</script>
+import http from '../http-common.js';
 
-<style>
-</style>
+class MovieDataService {
+    getAll() {
+        return http.get('/movie/');
+    }
+}
+export default {
+    name: "Series",
+    data() {
+        return {
+            movies: [],
+        };
+    },
+    methods: {
+        addMovies() {
+            const movieDataService = new MovieDataService
+            movieDataService
+                .getAll()
+                .then((response) => {
+                    this.movies = response.data;
+                    console.log(response.data)
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
+        },
+    },
+    mounted() {
+        this.addMovies();
+    },
+};
+</script>
