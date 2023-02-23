93dbd949b0a685580657c47169bc4102bfef9c19
Biademery <biademery@gmail.com>
2023-02-22 21:20:12 -0300

    change baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const http = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'https://git.heroku.com/stark-eyrie-43196.git/',
   headers: {
     'Content-type': 'application/json',
   },
