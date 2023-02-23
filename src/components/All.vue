99775c96eb5cb16a7ba46b6928105b830ea8226e
Biademery <biademery@gmail.com>
2023-02-22 21:08:38 -0300

    add all

---
+++ src/components/All.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="body">
+        <h2>Romance</h2>
+        <div class="cards">
+            <li v-for="(movie, index) in movies" :key="index">
+                <div>
+                    <a :href="movie.movieURL" target="_blank">
+                        <img class="image" :src="movie.imageURL" :alt="movie.name">
+                    </a>
+                </div>
+            </li>
+        </div>
+    </div>
+</template>
+  
+<script >
+import http from '../http-common.js';
+
+class MovieDataService {
+    getAll() {
+        return http.get('/movie/');
+    }
+}
+export default {
+    name: "All",
+    data() {
+        return {
+            movies: [],
+        };
+    },
+    methods: {
+        addMovies() {
+            const movieDataService = new MovieDataService
+            movieDataService
+                .getAll()
+                .then((response) => {
+                    this.movies = response.data;
+                    console.log(response.data)
+                })
+                .catch((e) => {
+                    console.log(e);
+                });
+        },
+    },
+    mounted() {
+        this.addMovies();
+    },
+};
+</script>
+  
