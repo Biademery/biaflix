6de8a41a4c0264906f0a6e1320155aa7cb1eac61
Biademery <biademery@gmail.com>
2023-02-22 21:07:59 -0300

    base http

---
+++ src/http-common.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: 'http://127.0.0.1:8000/',
+  headers: {
+    'Content-type': 'application/json',
+  },
+});
+
+export default http;
